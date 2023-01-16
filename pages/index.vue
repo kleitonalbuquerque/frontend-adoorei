8bc44c8b53b7daf7f0e3b6f48b0685413accf297
kleitonalbuquerque <kleiton2102@gmail.com>
2023-01-15 22:58:54 -0300

    [REFACT]Remoção de arquivos desnecessários

--- pages/index.vue
+++ pages/index.vue
@@ -233,14 +233,16 @@
                   >${{ produto.price }}</span
                 >
                 <a
+                  style="cursor: pointer;"
                   @click="adicionarItem"
                   class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-                  >Adicionar</a
+                  >Add to cart</a
                 >
                 <a
+                style="cursor: pointer;"
                   @click="fecharModal"
                   class="text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800"
-                  >Fechar</a
+                  >Close</a
                 >
               </div>
             </div>
